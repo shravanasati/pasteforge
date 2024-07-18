73a1486ec06d1a5ed1a185d513fb25136e2306e2
Shravan Asati <dev.shravan@protonmail.com>
2024-07-18 19:19:04 +0530

    env var configuration

--- backend/server.go
+++ backend/server.go
@@ -1,13 +1,59 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"regexp"
+
+	"github.com/joho/godotenv"
 
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 )
 
+// env vars
+var (
+	PORT string
+	GIN_MODE string
+	DIST_DIR string
+)
+
+func validateNotEmpty(key, value string) {
+	if value == "" {
+		panic(key + " env var not configured")
+	}
+}
+
+func validatePort(val string) {
+	matches, err := regexp.MatchString(`^\d{4,5}$`, val)
+	if err != nil {
+		panic("error validating port" +  err.Error())
+	}
+	if !matches {
+		panic(fmt.Sprintf("env var PORT=%s is incorrect", val))
+	}
+}
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("unable to load env variables")
+	}
+
+	PORT = os.Getenv("PORT")
+	validateNotEmpty("PORT", PORT)
+	validatePort(PORT)
+
+	GIN_MODE = os.Getenv("GIN_MODE")
+	validateNotEmpty("GIN_MODE", GIN_MODE)
+
+	DIST_DIR = os.Getenv("DIST_DIR")
+	validateNotEmpty("DIST_DIR", DIST_DIR)
+}
+
 func main() {
+	gin.SetMode(GIN_MODE)
 	router := gin.Default()
 	router.Use(gzip.Gzip(gzip.DefaultCompression))
 
@@ -22,5 +68,5 @@ func main() {
 	}
 	
 	router.NoRoute(gin.WrapH(http.FileServer(http.Dir("../dist"))))
-	router.Run(":3122")
+	router.Run(":" + PORT)
 }
