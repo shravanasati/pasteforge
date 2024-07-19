531b0b2f28513cb0bac8b7360c5f1f0b71669c26
Shravan Asati <dev.shravan@protonmail.com>
2024-07-19 22:12:56 +0530

    refactor paste settings code

--- src/components/PasteSettings.tsx
+++ src/components/PasteSettings.tsx
@@ -30,116 +30,126 @@ function validateSettings(settings: any): Settings {
   return settings
 }
 
+type ExpirationDropdownProps = {
+  expirationNumRef: React.RefObject<HTMLInputElement>
+  expirationDurationRef: React.RefObject<HTMLSelectElement>
+  setError: React.Dispatch<React.SetStateAction<string>>
+}
+
+function ExpirationDropdown({expirationNumRef, expirationDurationRef, setError}: ExpirationDropdownProps) {
+  return (
+    <div className="flex items-center">
+      <div tabIndex={0} role="button" className="text-primary m-2">expiration: </div>
+      <div
+        tabIndex={0}
+        className="text-primary-content z-[1] w-64 p-2 flex flex-row m-2">
+        <input type="number" min={0} placeholder="x" ref={expirationNumRef} className="input input-bordered w-full max-w-xs" onChange={(e) => {
+          if (!e.target.value) {
+            setError("")
+            return
+          }
+          const num = parseInt(e.target.value)
+          if (isNaN(num)) {
+            setError("expiration time must be a number")
+            return
+          }
+          if (num < 0) {
+            setError("expiration time cannot be negative")
+          } else if (num == 0 && expirationDurationRef.current!.value != "never") {
+            setError("expiration time must be greater than 0")
+          } else {
+            setError("")
+          }
+        }} />
+        <select className="select select-secondary w-full max-w-xs" ref={expirationDurationRef} onChange={(e) => {
+          if (e.target.value != "never" && expirationNumRef.current!.value == "0") {
+            setError("expiration time must be greater than 0")
+          } else {
+            setError("")
+          }
+        }} >
+          <option disabled >duration</option>
+          <option>minutes</option>
+          <option>hours</option>
+          <option>days</option>
+          <option>months</option>
+          <option>years</option>
+          <option selected>never</option>
+        </select>
+      </div>
+    </div>
+  )
+}
+
+type VisibilityProps = {
+  visibilityRef: React.RefObject<HTMLSelectElement>
+}
+
+function Visibility({visibilityRef}: VisibilityProps) {
+  return <div className="flex items-center">
+    <div tabIndex={0} role="button" className="text-primary m-2">visibility: </div>
+    <div
+      tabIndex={0}
+      className="text-primary-content z-[1] w-64 p-2 flex flex-row m-2">
+      <select className="select select-secondary w-full max-w-xs" ref={visibilityRef}>
+        <option selected>public</option>
+        <option>unlisted</option>
+        <option>private</option>
+      </select>
+    </div>
+  </div>
+}
+
+function PasswordField() {
+  return <div className="flex items-center">
+    <KeyRoundIcon size={24} className="mx-2 text-primary" />
+    <input type="password" placeholder="password" className="input input-bordered input-secondary w-full max-w-xs grow" />
+  </div>
+}
+
+function SettingsValidationError({error}: {error: string}) {
+  return <div role="alert" className="alert rounded alert-error w-auto">
+    <CircleX size={20} />
+    <span>{error}</span>
+  </div>
+}
+
+function handleSubmit(e: React.FormEvent, error: string, setError: React.Dispatch<React.SetStateAction<string>>, expirationNumRef: React.RefObject<HTMLInputElement>, expirationDurationRef: React.RefObject<HTMLSelectElement>, visibilityRef: React.RefObject<HTMLSelectElement>, setLoading: React.Dispatch<React.SetStateAction<boolean>>) {
+  e.preventDefault()
+  if (error) {
+    return
+  }
+  if (!expirationNumRef.current!.value && expirationDurationRef.current!.value != "never") {
+    setError("enter a number for expiration time")
+    return
+  } else {
+    setError("")
+  }
+  const settings: Settings = {
+    expiration: parseInt(expirationNumRef.current!.value),
+    duration: expirationDurationRef.current!.value as durationEnum,
+    visibility: visibilityRef.current!.value as visibilityEnum
+  }
+  try {
+    localStorage.setItem("settings", JSON.stringify(settings))
+  } catch (e) {
+    console.error("local storage quota exceeded")
+  }
+
+  // todo send to server
+  setLoading(true)
+  console.log("save button clicked")
+  setTimeout(() => setLoading(false), 2000)
+}
+
+
 export function PasteSettings() {
   const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
 
   const expirationNumRef = useRef<HTMLInputElement>(null)
   const expirationDurationRef = useRef<HTMLSelectElement>(null)
   const visibilityRef = useRef<HTMLSelectElement>(null)
-
-  function expirationDropdown() {
-    return (
-      <div className="flex items-center">
-        <div tabIndex={0} role="button" className="text-primary m-2">expiration: </div>
-        <div
-          tabIndex={0}
-          className="text-primary-content z-[1] w-64 p-2 flex flex-row m-2">
-          <input type="number" min={0} placeholder="x" ref={expirationNumRef} className="input input-bordered w-full max-w-xs" onChange={(e) => {
-            if (!e.target.value) {
-              setError("")
-              return
-            }
-            const num = parseInt(e.target.value)
-            if (isNaN(num)) {
-              setError("expiration time must be a number")
-              return
-            }
-            if (num < 0) {
-              setError("expiration time cannot be negative")
-            } else if (num == 0 && expirationDurationRef.current!.value != "never") {
-              setError("expiration time must be greater than 0")
-            } else {
-              setError("")
-            }
-          }} />
-          <select className="select select-secondary w-full max-w-xs" ref={expirationDurationRef} onChange={(e) => {
-            if (e.target.value != "never" && expirationNumRef.current!.value == "0") {
-              setError("expiration time must be greater than 0")
-            } else {
-              setError("")
-            }
-          }} >
-            <option disabled >duration</option>
-            <option>minutes</option>
-            <option>hours</option>
-            <option>days</option>
-            <option>months</option>
-            <option>years</option>
-            <option selected>never</option>
-          </select>
-        </div>
-      </div>
-    )
-  }
-
-  function visibility() {
-    return <div className="flex items-center">
-      <div tabIndex={0} role="button" className="text-primary m-2">visibility: </div>
-      <div
-        tabIndex={0}
-        className="text-primary-content z-[1] w-64 p-2 flex flex-row m-2">
-        <select className="select select-secondary w-full max-w-xs" ref={visibilityRef}>
-          <option selected>public</option>
-          <option>unlisted</option>
-          <option>private</option>
-        </select>
-      </div>
-    </div>
-  }
-
-  function password() {
-    return <div className="flex items-center">
-        <KeyRoundIcon size={24} className="mx-2 text-primary" />
-        <input type="password" placeholder="password" className="input input-bordered input-secondary w-full max-w-xs grow" /> 
-      </div>
-  }
-
-  function settingsValidationError() {
-    return <div role="alert" className="alert rounded alert-error w-auto">
-      <CircleX size={20} />
-      <span>{error}</span>
-    </div>
-  }
-
-  const [loading, setLoading] = useState(false)
-
-  function handleSubmit(e: React.FormEvent) {
-    e.preventDefault()
-    if (error) {
-      return
-    }
-    if (!expirationNumRef.current!.value && expirationDurationRef.current!.value != "never") {
-      setError("enter a number for expiration time")
-      return
-    } else {
-      setError("")
-    }
-    const settings: Settings = {
-      expiration: parseInt(expirationNumRef.current!.value),
-      duration: expirationDurationRef.current!.value as durationEnum,
-      visibility: visibilityRef.current!.value as visibilityEnum
-    }
-    try {
-      localStorage.setItem("settings", JSON.stringify(settings))
-    } catch (e) {
-      console.error("local storage quota exceeded")
-    }
-
-    // todo send to server
-    setLoading(true)
-    console.log("save button clicked")
-    setTimeout(() => setLoading(false), 2000)
-  }
 
   useEffect(() => {
     const settingsJSON = localStorage.getItem("settings")
@@ -156,13 +166,13 @@ export function PasteSettings() {
     visibilityRef.current!.value = settings.visibility
   }, [])
 
-  return <form onSubmit={handleSubmit}>
+  return <form onSubmit={(e) => handleSubmit(e, error, setError, expirationNumRef, expirationDurationRef, visibilityRef, setLoading)}>
     <h1 className="m-2 p-2">paste settings</h1>
     <div className="flex flex-row justify-start flex-wrap items-center ml-4">
-      {expirationDropdown()}
-      {visibility()}
-      {password()}
-      {error == "" ? null : settingsValidationError()}
+      <ExpirationDropdown expirationNumRef={expirationNumRef} expirationDurationRef={expirationDurationRef} setError={setError} />
+      <Visibility visibilityRef={visibilityRef} />
+      <PasswordField />
+      {error == "" ? null : <SettingsValidationError error={error}/>}
     </div>
     {!loading ? <input type="submit" value="save" className="btn m-4" /> : <span className="btn m-4 loading loading-dots text-primary" />}
   </form>
